26ca68c9ee5566ce1c43a165c96b3cee423b3621
mark <mark@tkkrlab.nl>
2020-05-01 22:53:07 +0200

    Changed pin variables to defines. removed some unused code.

--- Linear_Motor_Steuerung.ino
+++ Linear_Motor_Steuerung.ino
@@ -5,13 +5,16 @@
 
 volatile int encoder0Pos = 0;
 
-const int EN1 = 5;
-const int EN2 = 6;
-const int EN3 = 7;
+#define EN1 5
+#define EN2 6
+#define EN3 7
  
-const int IN1 = 9;
-const int IN2 = 10;
-const int IN3 = 11;
+#define IN1 9
+#define IN2 10
+#define IN3 11
+
+#define DirPin 4
+#define StepPin 12
 
 double Setpoint, Input, Output;
 PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
@@ -22,35 +25,24 @@ PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
 // SVPWM (Space Vector Wave)
 const int pwmSin[] = {128, 132, 136, 140, 143, 147, 151, 155, 159, 162, 166, 170, 174, 178, 181, 185, 189, 192, 196, 200, 203, 207, 211, 214, 218, 221, 225, 228, 232, 235, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 235, 232, 228, 225, 221, 218, 214, 211, 207, 203, 200, 196, 192, 189, 185, 181, 178, 174, 170, 166, 162, 159, 155, 151, 147, 143, 140, 136, 132, 128, 124, 120, 116, 113, 109, 105, 101, 97, 94, 90, 86, 82, 78, 75, 71, 67, 64, 60, 56, 53, 49, 45, 42, 38, 35, 31, 28, 24, 21, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 24, 28, 31, 35, 38, 42, 45, 49, 53, 56, 60, 64, 67, 71, 75, 78, 82, 86, 90, 94, 97, 101, 105, 109, 113, 116, 120, 124};
 
+#define ARRAYSIZE sizeof(pwmSin)/sizeof(int)
 
 enum phase {PHASE1, PHASE2, PHASE3};
 int phasePwmIndex[3];
 
-/*
-int *pos_U = pwmSin;
-int *pos_V = pwmSin + 24;
-int *pos_W = pwmSin + 48;
-*/
 int *pos_U, *pos_V, *pos_W;
 
-
-
-int directionPin = 4;
-int stepPin = 12;
-
-int test;
 int commutationPWMOffset = 24;
 
 //This distance is the difference between two magnets with the same polarity.
 const double phaseDistance = 27;
 
-double position_in_mm = (encoder0Pos) / 40.00;
+double position_in_mm = 0; // (encoder0Pos) / 40.00;
+String inString = "";
 
-
- 
 void setup() 
 {
-  commutationPWMOffset = sizeof(pwmSin)/sizeof(int);
+  commutationPWMOffset = ARRAYSIZE/3;
 
   phasePwmIndex[0] = 0;
   phasePwmIndex[1] = commutationPWMOffset;
@@ -67,7 +59,7 @@ void setup()
   attachInterrupt(1, isrB, CHANGE);
 
   //Increase PWM frequency to 32 kHz  (make inaudible)
-  setPwmFrequency(IN1); 
+  setPwmFrequency(IN1);
   setPwmFrequency(IN2);
   setPwmFrequency(IN3);
 
@@ -103,7 +95,7 @@ void setup()
   Serial.begin(115200);
 }
 
-String inString = "";
+
 
 void loop()
 {
@@ -132,26 +124,25 @@ void drive(double scale_factor)
 {
   if(scale_factor < 0.00)
   {
-    double  temp = -scale_factor;
     //linear scale Y = x * (ymax - ymin)/(xmax - xmin) + offset
-    temp = (temp - 0.00) * (1.0 - 0.1) / (1.00 - 0.00) + 0.1;
+    scale_factor = (-scale_factor - 0.00) * (1.0 - 0.1) / (1.00 - 0.00) + 0.1;
     
-    test = -(phasePwmIndex[0] - berechne() + commutationPWMOffset);
-    shift(&pos_U, test, 72, PHASE1);
-    shift(&pos_V, test, 72, PHASE2);
-    shift(&pos_W, test, 72, PHASE3);
+    int offset = -(phasePwmIndex[0] - berechne() + commutationPWMOffset);
+    shift(&pos_U, offset, ARRAYSIZE, PHASE1);
+    shift(&pos_V, offset, ARRAYSIZE, PHASE2);
+    shift(&pos_W, offset, ARRAYSIZE, PHASE3);
   
-    analogWrite(IN1,*pos_U * temp);
-    analogWrite(IN2,*pos_V * temp);
-    analogWrite(IN3,*pos_W * temp);
+    analogWrite(IN1,*pos_U * scale_factor);
+    analogWrite(IN2,*pos_V * scale_factor);
+    analogWrite(IN3,*pos_W * scale_factor);
   }
   else if(scale_factor > 0.00)
   {
-    test = (phasePwmIndex[0] + berechne() + commutationPWMOffset);
     scale_factor = (scale_factor - 0.00) * (1.0 - 0.1) / (1.00 - 0.00) + 0.1;
-    shift(&pos_U, test, 72, PHASE1);
-    shift(&pos_V, test, 72, PHASE2);
-    shift(&pos_W, test, 72, PHASE3);
+    int offset = (phasePwmIndex[0] + berechne() + commutationPWMOffset);
+    shift(&pos_U, offset, ARRAYSIZE, PHASE1);
+    shift(&pos_V, offset, ARRAYSIZE, PHASE2);
+    shift(&pos_W, offset, ARRAYSIZE, PHASE3);
     
     analogWrite(IN1,*pos_U * scale_factor);
     analogWrite(IN2,*pos_V * scale_factor);
